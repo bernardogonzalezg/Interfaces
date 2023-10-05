441a56345af92aaef531441f290c8a2ca25a9896
Bernardo Gonzalez <bgonzalezgirodo@alumnos.exa.unicen.edu.ar>
2023-10-04 21:07:26 -0300

    registro con animacion

--- tp2/js/registro.js
+++ tp2/js/registro.js
@@ -1,13 +1,35 @@
 "use strict"
 
-var x = document.getElementById("registro");
-var y = document.getElementById("login");
+const registroButton = document.querySelector('.registro');
+const inicioSesionButton = document.querySelector('.inicio-sesion');
 
-document.querySelector(".inicio-sesion").addEventListener("click", mostrarLogueo);
-document.querySelector(".inicio-sesion").addEventListener("click", mostrarLogueo);
+const registroForm = document.getElementById('registro');
+const loginForm = document.getElementById('login');
 
-function mostrarLogueo() {
-    document.querySelector(".form-box").classList.toggle("nomostrar");
+inicioSesionButton.addEventListener('click', () => {
+    registroForm.style.display = 'none';
+    loginForm.style.display = 'block';
+    window.scrollTo(0, 0);
+});
 
+registroButton.addEventListener('click', () => {
+    loginForm.style.display = 'none';
+    registroForm.style.display = 'block';
+    window.scrollTo(0, 0);
+});
+
+document.querySelector(".btn-registro").addEventListener("click", mostrarMensajeExito);
+
+// Después de un registro exitoso, muestra el mensaje de confirmación
+function mostrarMensajeExito() {
+    const mensajeExito = document.getElementById('registro-exitoso');
+    mensajeExito.style.display = 'block';
+
+    // Oculta el mensaje después de 3 segundos
+    setTimeout(function () {
+        mensajeExito.style.display = 'none';
+
+        // Redirige a la página deseada
+        window.location.href = 'home.html'; // Cambia 'otra_pagina.html' por la URL a la que deseas redirigir al usuario
+    }, 3000); // 3000 milisegundos = 3 segundos
 }
-
