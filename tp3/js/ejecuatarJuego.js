3e2bfa5241e414c3d300403364d3b01f852e75b8
juan manuel lahorca <juanmanuellahorca@gmail.com>
2023-11-03 15:51:18 -0300

    temporizador y ciclo en juego incrustado

--- tp3/js/ejecuatarJuego.js
+++ tp3/js/ejecuatarJuego.js
@@ -1,11 +1,35 @@
 "use strict"
-let conteinerEjecutar = document.querySelector("#juego-ejecutado");
-conteinerEjecutar.classList.add("container-inactivo");
-let btnEjecutar = document.querySelector("#btn-ejecutar").addEventListener("click", renderizarJuego);
 
-function renderizarJuego() {
-    let conteinerBtnJuegoEjecutar = document.querySelector("#conteiner-btn-juegoEjecutar");
-    conteinerEjecutar.classList.add("container-activo");
-    conteinerEjecutar.classList.remove("container-inactivo");
-    conteinerBtnJuegoEjecutar.classList.add("container-inactivo");
+let conteinerJuego = document.querySelector("#juego-ejecutado");
+let conteinerOpciones = document.querySelector("#game-settings");
+let conteinerInicial = document.querySelector("#conteiner-btn-juegoEjecutar");
+
+//desactiva al cargar el juego y las opciones de juego
+conteinerJuego.classList.add("container-inactivo");
+conteinerOpciones.classList.add("container-inactivo");
+
+//activa las opciones, desactiva la presentacion
+function irOpciones() {
+    conteinerOpciones.classList.add("container-activo");
+    conteinerOpciones.classList.remove("container-inactivo");
+    conteinerInicial.classList.add("container-inactivo");
 }
+ //desactiva las opciones y activa el juego
+function renderizarJuego(){
+    conteinerJuego.classList.add("container-activo");
+    conteinerJuego.classList.remove("container-inactivo");
+    conteinerOpciones.classList.add("container-inactivo");
+    conteinerOpciones.classList.remove("container-activo");
+}
+
+//desactiva el juego y activa la presentacion
+function salirJuego(){
+    conteinerJuego.classList.add("container-inactivo");
+    conteinerJuego.classList.remove("container-activo");
+    conteinerInicial.classList.add("container-activo");
+    conteinerInicial.classList.remove("container-inactivo");
+}
+
+document.querySelector("#btn-ejecutar").addEventListener("click", irOpciones);
+document.querySelector("#play-settings-btn").addEventListener("click", renderizarJuego);
+document.querySelector("#close-btn").addEventListener("click", salirJuego);
