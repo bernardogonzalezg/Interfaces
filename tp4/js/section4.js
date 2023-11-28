1d1884a337322218e2c8500c1d203b3df7866bac
juan manuel lahorca <juanmanuellahorca@gmail.com>
2023-11-28 16:52:27 -0300

    section4 actualizado

--- tp4/js/section4.js
+++ tp4/js/section4.js
@@ -4,16 +4,6 @@ let card03 = document.getElementById('section-4-card3');
 
 let section4 = document.getElementById('section4');
 
-window.addEventListener('scroll', function(e){
-    let rect = section4.getBoundingClientRect();
-    if (rect.top+100 <= window.innerHeight && rect.bottom-100 >= 0) {
-        let scroll = window.scrollY;
-        card01.style.transform = 'translateX(calc(-65% + ' + scroll *0.15 + 'px))';
-        card02.style.transform = 'translateX(calc(-65% + ' + scroll *0.15 + 'px))';
-        card03.style.transform = 'translateX(calc(-65% + ' + scroll *0.15 + 'px))';   
-    }
-});
-
 card01.addEventListener('mouseenter', function() {
     card01.style.transform = 'skewY(7deg)';
     card01.style.transition = 'all 0.5s ease-in-out';
@@ -39,7 +29,7 @@ card03.addEventListener('mouseenter', function() {
     card03.style.transition = 'all 0.5s ease-in-out';
 });
 
-card3.addEventListener('mouseleave', function() {
-    card3.style.transform = 'none';
-    card3.style.transition = 'none';
+card03.addEventListener('mouseleave', function() {
+    card03.style.transform = 'none';
+    card03.style.transition = 'none';
 });
